9e3459b14fa09eddfcbabb92ac61554bba653963
Vegar Westerlund <vegarwe@gmail.com>
2014-03-29 00:55:33 +0100

    Is now connecting...

--- tls/src/main/java/no/raiom/tls/TempLogDeviceAction.java
+++ tls/src/main/java/no/raiom/tls/TempLogDeviceAction.java
@@ -7,9 +7,13 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.util.Log;
+
+import java.util.UUID;
 
 public class TempLogDeviceAction {
     private KeyguardManager.KeyguardLock mKeyGuardLock;
@@ -32,7 +36,10 @@ public class TempLogDeviceAction {
                     Log.e("Fisken", "Found device: "
                             + device.getName() + " " + device.getAddress());
                     if (device.getName().equals("TLS_480204226")) {
-                        device.connectGatt(mContext, true, mGattCallback);
+                        if (!found_device) {
+                            device.connectGatt(mContext, true, mGattCallback);
+                            found_device = true;
+                        }
                     }
                 }
             };
@@ -52,6 +59,23 @@ public class TempLogDeviceAction {
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             Log.i("Fisken", "onServicesDiscovered");
+            for (BluetoothGattService srv : gatt.getServices()) {
+                Log.i("Fisken", "srv:      " + srv.getUuid().toString() + " - ");
+
+                for (BluetoothGattCharacteristic chr : srv.getCharacteristics()) {
+                    Log.i("Fisken", ". chr:    " + chr.getUuid().toString());
+
+                    for (BluetoothGattDescriptor dscr : chr.getDescriptors()) {
+                        Log.i("Fisken", ".   dscr: " + dscr.getUuid().toString());
+                        if (UUID.fromString("000018fb-0000-1000-8000-00805f9b34fb").equals(chr.getUuid())) {
+                            Log.i("Fisken", "Enabeling services");
+                            gatt.setCharacteristicNotification(chr, true);
+                            dscr.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+                            gatt.writeDescriptor(dscr);
+                        }
+                    }
+                }
+            }
         }
 
         @Override
@@ -65,6 +89,7 @@ public class TempLogDeviceAction {
         public void onCharacteristicChanged(BluetoothGatt gatt,
                                             BluetoothGattCharacteristic characteristic) {
             Log.i("Fisken", "onCharacteristicChanged");
+            gatt.disconnect();
         }
     };
 }
