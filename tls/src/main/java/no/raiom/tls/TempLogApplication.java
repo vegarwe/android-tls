ff80367490117232a7e8bbd944b1fab980af7ab9
Vegar Westerlund <vegarwe@gmail.com>
2014-06-30 22:31:07 +0200

    Start work on a device config singleton placed in the application class

---
+++ tls/src/main/java/no/raiom/tls/TempLogApplication.java
@@ -0,0 +1,11 @@
+package no.raiom.tls;
+
+import android.app.Application;
+
+/**
+ * Created by vegarwe on 18.05.2014.
+ */
+public class TempLogApplication extends Application {
+
+    public DeviceConfig deviceConfig;
+}
