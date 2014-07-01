e271a79e0cc76869c30030b28bfcfff5572698ac
Vegar Westerlund <vegarwe@gmail.com>
2014-07-01 23:58:58 +0200

    Start handling the TempLog service properly. Need to read out two metadata characteristics before it makes sense to enable service

--- tls/src/main/java/no/raiom/tls/TempLogDeviceAction.java
+++ tls/src/main/java/no/raiom/tls/TempLogDeviceAction.java
@@ -14,13 +14,33 @@ import java.util.List;
 import java.util.UUID;
 
 public class TempLogDeviceAction {
-    public final static UUID TEMP_LOG = UUID.fromString("000018fb-0000-1000-8000-00805f9b34fb");
+    public final static UUID TLS_VALUE = UUID.fromString("000018fb-0000-1000-8000-00805f9b34fb");
+    public final static UUID TLS_DESC1 = UUID.fromString("000018fc-0000-1000-8000-00805f9b34fb");
+    public final static UUID TLS_DESC2 = UUID.fromString("000018fd-0000-1000-8000-00805f9b34fb");
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
 
     public void connect(Context context, BluetoothDevice device) {
         device.connectGatt(context, false, leGattCallback);
     }
 
     private final BluetoothGattCallback leGattCallback = new BluetoothGattCallback() {
+
+        BluetoothGattCharacteristic value_chr = null;
+        BluetoothGattCharacteristic desc1_chr = null;
+        BluetoothGattCharacteristic desc2_chr = null;
+        BluetoothGattDescriptor    value_dscr = null;
+        TempLog                           tls = null;
+
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             if (status != BluetoothGatt.GATT_SUCCESS) {
@@ -42,26 +62,66 @@ public class TempLogDeviceAction {
 
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status != BluetoothGatt.GATT_SUCCESS) {
+                Log.w("Fisken", "onServicesDiscovered received: " + status);
+                return;
+            }
+
             for (BluetoothGattService srv : gatt.getServices()) {
                 for (BluetoothGattCharacteristic chr : srv.getCharacteristics()) {
-                    for (BluetoothGattDescriptor dscr : chr.getDescriptors()) {
-                        if (TEMP_LOG.equals(chr.getUuid())) {
-                            gatt.setCharacteristicNotification(chr, true);
-                            dscr.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
-                            gatt.writeDescriptor(dscr);
+                    Log.i("Fisken", "UUID: " + chr.getUuid());
+                    if (TLS_VALUE.equals(chr.getUuid())) {
+                        value_chr = chr;
+                        for (BluetoothGattDescriptor dscr : chr.getDescriptors()) {
+                            value_dscr = dscr;
                         }
+                    } else if (TLS_DESC1.equals(chr.getUuid())) {
+                        desc1_chr = chr;
+                    } else if (TLS_DESC2.equals(chr.getUuid())) {
+                        desc2_chr = chr;
                     }
                 }
+            }
+
+            if (value_dscr != null && desc1_chr != null && desc2_chr != null) {
+                gatt.readCharacteristic(desc1_chr);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                            BluetoothGattCharacteristic chr,
+                                            int status) {
+            if (status != BluetoothGatt.GATT_SUCCESS) {
+                Log.w("Fisken", "onServicesDiscovered received: " + status);
+                return;
+            }
+
+            Log.d("Fisken", "Read " + chr + " UUID " + chr.getUuid() + " value: " + bytesToHex(chr.getValue()));
+            if (TLS_DESC1.equals(chr.getUuid())) {
+                gatt.readCharacteristic(desc2_chr);
+            } else if (TLS_DESC2.equals(chr.getUuid())) {
+                long now = System.currentTimeMillis();
+                tls = TempLog.from_byte_data(now, desc1_chr.getValue(), desc2_chr.getValue());
+                gatt.setCharacteristicNotification(value_chr, true);
+                value_dscr.setValue(BluetoothGattDescriptor.ENABLE_INDICATION_VALUE);
+                gatt.writeDescriptor(value_dscr);
             }
         }
 
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt,
                                             BluetoothGattCharacteristic characteristic) {
-            if (TEMP_LOG.equals(characteristic.getUuid())) {
-                List<TempLogSample> samples = TempLogSample.decode_samples(characteristic.getValue());
-                for (TempLogSample s : samples) {
-                    Log.i("Fisken", "Sample: " + s);
+            if (TLS_VALUE.equals(characteristic.getUuid())) {
+
+                List<TempLog.TempLogSample> samples = tls.decode_samples(characteristic.getValue());
+                for (TempLog.TempLogSample s : samples) {
+                    StringBuilder sl = new StringBuilder();
+                    sl.append(" rand: ");
+                    sl.append(bytesToHex(tls.random));
+                    sl.append(" sample: ");
+                    sl.append(s);
+                    Log.i("Fisken", sl.toString());
                 }
             }
         }
