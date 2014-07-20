46c1ffb52a67f2f1decea2bc8111bf65899f3b5e
Vegar Westerlund <vegarwe@gmail.com>
2014-07-20 23:05:06 +0200

    Make a weak singelton of the Le scanner and make it available from the AppConfig object

--- app/src/main/java/no/raiom/tls/AppConfig.java
+++ app/src/main/java/no/raiom/tls/AppConfig.java
@@ -10,9 +10,18 @@ public class AppConfig {
     public static final String APP_KEY    = "3f0zvk7t1aib5dd";
     public static final String APP_SECRET = "tvdzxfe1dkpd694";
 
+    public TempLogScanner scanner = null;
+
     private AppConfig(Context context) {
         app_context = context;
         this.deviceConfig = new TempLogDeviceConfig();
+    }
+
+    public TempLogScanner getScanner() {
+        if (scanner == null) {
+            scanner = new TempLogScanner(this);
+        }
+        return scanner;
     }
 
     public static AppConfig getInstance(Context context) {
