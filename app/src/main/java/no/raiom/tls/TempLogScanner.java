58e0804fcca5806da3b2240ab025a834e6b1b6c7
Vegar Westerlund <vegarwe@gmail.com>
2014-07-23 22:52:27 +0200

    Implement handling for bluetooth adapter being turned off (and on again)

--- app/src/main/java/no/raiom/tls/TempLogScanner.java
+++ app/src/main/java/no/raiom/tls/TempLogScanner.java
@@ -8,8 +8,10 @@ import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanResult;
 import android.bluetooth.le.ScanSettings;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.ParcelUuid;
 import android.util.Log;
 
@@ -25,10 +27,12 @@ public class TempLogScanner {
 
     public TempLogScanner(AppConfig config) {
         this.config              = config;
-        BluetoothManager manager = (BluetoothManager)config.context.getSystemService(Context.BLUETOOTH_SERVICE);
-        BluetoothAdapter adapter = manager.getAdapter();
-        scanner                  = adapter.getBluetoothLeScanner();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        config.context.registerReceiver(bluetoothStatusChangeReceiver, filter);
     }
+
 
     public void startScan() {
         // TODO: Check if bluetooth is enabled!
@@ -49,18 +53,13 @@ public class TempLogScanner {
         // Start scan
         Log.i("Fisken", "TempLogScanner.startScan: settings " + settings + " filters " + filters + " leScanCallback " + leScanCallback);
         getScanner().startScan(filters, settings, leScanCallback);
+        Log.i("Fisken", "getScanner: " + getScanner());
     }
 
     public void stopScan() {
         Log.i("Fisken", "TempLogScanner.stopScan: " + leScanCallback);
         getScanner().stopScan(leScanCallback);
-    }
-
-    private BluetoothLeScanner getScanner() {
-        //BluetoothManager  manager = (BluetoothManager)config.context.getSystemService(Context.BLUETOOTH_SERVICE);
-        //BluetoothAdapter  adapter = manager.getAdapter();
-        //return adapter.getBluetoothLeScanner();
-        return scanner;
+        Log.i("Fisken", "getScanner: " + getScanner());
     }
 
     private ScanCallback leScanCallback =
@@ -68,19 +67,19 @@ public class TempLogScanner {
 
                 @Override
                 public void onAdvertisementUpdate(ScanResult result) {
-                    //BluetoothDevice device = result.getDevice();
-                    //Log.i("Fisken", "onAdvertisementUpdate " + result + " device: " + device);
+                    BluetoothDevice device = result.getDevice();
+                    Log.i("Fisken", "onAdvertisementUpdate " + result + " device: " + device);
 
-                    //TempLogDeviceConfig.Device confDevice = config.deviceConfig.getDevice(device.getAddress());
-                    //if (confDevice == null) {
-                    //    confDevice = TempLogDeviceConfig.Device.fromScanResult(result);
-                    //    config.deviceConfig.add(confDevice);
+                    TempLogDeviceConfig.Device confDevice = config.deviceConfig.getDevice(device.getAddress());
+                    if (confDevice == null) {
+                        confDevice = TempLogDeviceConfig.Device.fromScanResult(result);
+                        config.deviceConfig.add(confDevice);
 
-                    //    Log.i("Fisken", "New device");
-                    //    //Intent service = new Intent(config.app_context, TempLogProfile.class);
-                    //    //service.putExtra("device_addr", device.getAddress());
-                    //    //config.app_context.startService(service);
-                    //}
+                        Log.i("Fisken", "New device");
+                        //Intent service = new Intent(config.app_context, TempLogProfile.class);
+                        //service.putExtra("device_addr", device.getAddress());
+                        //config.app_context.startService(service);
+                    }
                 }
 
                 @Override
@@ -88,4 +87,33 @@ public class TempLogScanner {
                     Log.e("Fisken", "onScanFailed " + errorCode);
                 }
             };
+
+    private final BroadcastReceiver bluetoothStatusChangeReceiver =
+            new BroadcastReceiver() {
+
+                public void onReceive(Context context, Intent intent) {
+                    String action = intent.getAction();
+                    if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)){
+                        if (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_OFF) {
+                            resetScanner();
+                        //} else if(intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1) == BluetoothAdapter.STATE_ON){
+                        //    initScanner();
+                        }
+                    }
+                }
+            };
+
+    private BluetoothLeScanner getScanner() {
+        if (scanner == null) {
+            BluetoothManager manager = (BluetoothManager)config.context.getSystemService(Context.BLUETOOTH_SERVICE);
+            BluetoothAdapter adapter = manager.getAdapter();
+            scanner                  = adapter.getBluetoothLeScanner();
+        }
+        return scanner;
+    }
+
+    private void resetScanner() {
+        Log.e("Fisken", "resetScanner");
+        scanner = null;
+    }
 }
