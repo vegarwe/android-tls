b3a255cfcf95fa59a6f9d5bbb921a618da75d1a9
Vegar Westerlund <vegarwe@gmail.com>
2014-07-23 21:59:24 +0200

    Remove explicit use of android.bluetooth.le for all .le classes. No longer needed with latest Android studio. We need to keep the same instance of BluetoothLeScanner for turning on an off. Need to handle DeadObject some other way.

--- app/src/main/java/no/raiom/tls/TempLogScanner.java
+++ app/src/main/java/no/raiom/tls/TempLogScanner.java
@@ -3,73 +3,84 @@ package no.raiom.tls;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
 import android.os.ParcelUuid;
 import android.util.Log;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+
 public class TempLogScanner {
     private final static UUID                       TLS_SERVI = UUID.fromString("000018fa-0000-1000-8000-00805f9b34fb");
-    private android.bluetooth.le.BluetoothLeScanner scanner;
-    private Map<String, Boolean>                    addrs;
     private AppConfig                               config;
-
+    private BluetoothLeScanner scanner;
 
     public TempLogScanner(AppConfig config) {
-        this.config               = config;
-        this.addrs                = new HashMap<String, Boolean>();
-        BluetoothManager  manager = (BluetoothManager)config.app_context.getSystemService(Context.BLUETOOTH_SERVICE);
-        BluetoothAdapter  adapter = manager.getAdapter();
-        scanner = adapter.getBluetoothLeScanner();
+        this.config              = config;
+        BluetoothManager manager = (BluetoothManager)config.context.getSystemService(Context.BLUETOOTH_SERVICE);
+        BluetoothAdapter adapter = manager.getAdapter();
+        scanner                  = adapter.getBluetoothLeScanner();
     }
 
     public void startScan() {
         // TODO: Check if bluetooth is enabled!
 
         // Filters
-        List<android.bluetooth.le.ScanFilter> filters = new ArrayList<android.bluetooth.le.ScanFilter>();
-        android.bluetooth.le.ScanFilter.Builder filter_builder = new android.bluetooth.le.ScanFilter.Builder();
+        List<ScanFilter> filters = new ArrayList<ScanFilter>();
+        ScanFilter.Builder filter_builder = new ScanFilter.Builder();
         filter_builder.setServiceUuid(new ParcelUuid(TLS_SERVI));
         filter_builder.setServiceData(ByteUtils.hexStringToByteArray("FE1801"));
         filters.add(filter_builder.build());
 
         // Settings
-        android.bluetooth.le.ScanSettings.Builder builder = new android.bluetooth.le.ScanSettings.Builder();
-        builder.setCallbackType(android.bluetooth.le.ScanSettings.CALLBACK_TYPE_ON_UPDATE);
-        builder.setScanMode(android.bluetooth.le.ScanSettings.SCAN_MODE_LOW_POWER);
-        android.bluetooth.le.ScanSettings settings = builder.build();
+        ScanSettings.Builder builder = new ScanSettings.Builder();
+        builder.setCallbackType(ScanSettings.CALLBACK_TYPE_ON_UPDATE);
+        builder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER);
+        ScanSettings settings = builder.build();
 
         // Start scan
         Log.i("Fisken", "TempLogScanner.startScan: settings " + settings + " filters " + filters + " leScanCallback " + leScanCallback);
-        scanner.startScan(filters, settings, leScanCallback);
+        getScanner().startScan(filters, settings, leScanCallback);
     }
 
     public void stopScan() {
-        Log.i("Fisken", "TempLogScanner.stopScan");
-        scanner.stopScan(leScanCallback);
+        Log.i("Fisken", "TempLogScanner.stopScan: " + leScanCallback);
+        getScanner().stopScan(leScanCallback);
     }
 
-    private android.bluetooth.le.ScanCallback leScanCallback =
-            new android.bluetooth.le.ScanCallback() {
+    private BluetoothLeScanner getScanner() {
+        //BluetoothManager  manager = (BluetoothManager)config.context.getSystemService(Context.BLUETOOTH_SERVICE);
+        //BluetoothAdapter  adapter = manager.getAdapter();
+        //return adapter.getBluetoothLeScanner();
+        return scanner;
+    }
+
+    private ScanCallback leScanCallback =
+            new ScanCallback() {
 
                 @Override
-                public void onAdvertisementUpdate(android.bluetooth.le.ScanResult result) {
-                    BluetoothDevice device = result.getDevice();
-                    Log.i("Fisken", "onAdvertisementUpdate " + result + " device: " + device);
-                    if (! addrs.containsKey(device.getAddress())) {
-                        addrs.put(device.getAddress(), true);
+                public void onAdvertisementUpdate(ScanResult result) {
+                    //BluetoothDevice device = result.getDevice();
+                    //Log.i("Fisken", "onAdvertisementUpdate " + result + " device: " + device);
 
-                        Log.i("Fisken", "New device");
-                        //Intent service = new Intent(config.app_context, TempLogProfile.class);
-                        //service.putExtra("device_addr", device.getAddress());
-                        //config.app_context.startService(service);
-                    }
+                    //TempLogDeviceConfig.Device confDevice = config.deviceConfig.getDevice(device.getAddress());
+                    //if (confDevice == null) {
+                    //    confDevice = TempLogDeviceConfig.Device.fromScanResult(result);
+                    //    config.deviceConfig.add(confDevice);
+
+                    //    Log.i("Fisken", "New device");
+                    //    //Intent service = new Intent(config.app_context, TempLogProfile.class);
+                    //    //service.putExtra("device_addr", device.getAddress());
+                    //    //config.app_context.startService(service);
+                    //}
                 }
 
                 @Override
