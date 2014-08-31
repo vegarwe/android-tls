fbbcc94e30c19530f6b2aac1c0dcda65a2e81b83
Vegar Westerlund <vegarwe@gmail.com>
2014-08-31 19:08:38 +0200

    Refining instal script

--- adb_install_and_run.bat
+++ adb_install_and_run.bat
@@ -6,5 +6,7 @@ rem gradlew.bat assembleDebug
 "\Program Files (x86)\Android\android-studio\sdk\platform-tools\adb.exe" shell ^
     am start -n "no.raiom.tls/no.raiom.tls.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER; ^
     logcat -c
-"\Program Files (x86)\Android\android-studio\sdk\platform-tools\adb.exe" logcat |find "Fisken"
+echo "app installed, piping log to file \tmp\logcat.txt"
+"\Program Files (x86)\Android\android-studio\sdk\platform-tools\adb.exe" logcat -v time >> \tmp\logcat.txt
+"\Program Files (x86)\Android\android-studio\sdk\platform-tools\adb.exe" logcat -c
 
