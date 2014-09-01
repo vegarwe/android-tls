203c1acaca22a276727049c6dc40f40622e91c34
Vegar Westerlund <vegarwe@gmail.com>
2014-09-01 19:29:45 +0200

    Clean up App conig singelton

--- app/src/main/java/no/raiom/tls/AppConfig.java
+++ app/src/main/java/no/raiom/tls/AppConfig.java
@@ -3,23 +3,16 @@ package no.raiom.tls;
 import android.content.Context;
 
 public class AppConfig {
-    private static AppConfig app_instance = null;
+    private static AppConfig instance = null;
 
-    public TempLogDeviceConfig deviceConfig;
-    public Context             context;
     public static final String APP_KEY    = "3f0zvk7t1aib5dd";
     public static final String APP_SECRET = "tvdzxfe1dkpd694";
 
-    private AppConfig(Context context) {
-        this.context = context;
-        this.deviceConfig = new TempLogDeviceConfig();
-    }
-
-    public static AppConfig getInstance(Context context) {
-        if (AppConfig.app_instance == null) {
-            AppConfig.app_instance = new AppConfig(context.getApplicationContext());
+    public static AppConfig getInstance() {
+        if (AppConfig.instance == null) {
+            AppConfig.instance = new AppConfig();
         }
 
-        return app_instance;
+        return instance;
     }
 }
